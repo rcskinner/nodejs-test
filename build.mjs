de497d3c49e2d47a2c990fe0a01eeb47025344b6
rcskinner <ryan.skinner@datadoghq.com>
2025-03-03 10:09:34 -0700

    Bundling dd_trace and changing some of the esbuild

--- build.mjs
+++ build.mjs
@@ -1,12 +1,36 @@
-import { build } from "esbuild";
+import esbuild from 'esbuild';
+import ddPlugin from 'dd-trace/esbuild.js';
 
-build({
+esbuild.build({
   entryPoints: ["src/index.js"],
   outfile: "dist/bundle.js",
   platform: "node",
   format: "esm",
   bundle: true,
   sourcemap: true,
+  plugins: [ddPlugin],
+  external: [
+    // required if you use native metrics
+    '@datadog/native-metrics',
+
+    // required if you use profiling
+    '@datadog/pprof',
+
+    // required if you use Datadog security features
+    '@datadog/native-appsec',
+    '@datadog/native-iast-taint-tracking',
+    '@datadog/native-iast-rewriter',
+
+    // required if you encounter graphql errors during the build step
+    'graphql/language/visitor',
+    'graphql/language/printer',
+    'graphql/utilities',
+    
+    //External App Dependencies 
+    'express',
+    'pino',
+
+  ]
 })
   .then(() => console.log("Build successful!"))
   .catch(() => process.exit(1));
